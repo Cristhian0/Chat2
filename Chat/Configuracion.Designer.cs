7f661e5bad8ee79c9418f142357e7c219a9f8b90
mateo ortiz <mateo_ortiz08@hotmail.com>
2017-06-29 10:39:54 -0500

    mas

--- Chat/Configuracion.Designer.cs
+++ Chat/Configuracion.Designer.cs
@@ -36,6 +36,7 @@
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -43,74 +44,82 @@
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(46, 41);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.Size = new System.Drawing.Size(73, 13);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "label1";
+			this.label1.Text = "HORA INICIO";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Location = new System.Drawing.Point(46, 76);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.Size = new System.Drawing.Size(58, 13);
 			this.label2.TabIndex = 1;
-			this.label2.Text = "label2";
+			this.label2.Text = "HORA FIN";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Location = new System.Drawing.Point(46, 110);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(35, 13);
+			this.label3.Size = new System.Drawing.Size(32, 13);
 			this.label3.TabIndex = 2;
-			this.label3.Text = "label3";
+			this.label3.Text = "DIAS";
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(132, 165);
+			this.label4.Location = new System.Drawing.Point(119, 164);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(35, 13);
+			this.label4.Size = new System.Drawing.Size(97, 13);
 			this.label4.TabIndex = 3;
-			this.label4.Text = "label4";
+			this.label4.Text = "MENSAJE INICIAL";
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(132, 251);
+			this.label5.Location = new System.Drawing.Point(119, 251);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(35, 13);
+			this.label5.Size = new System.Drawing.Size(90, 13);
 			this.label5.TabIndex = 4;
-			this.label5.Text = "label5";
+			this.label5.Text = "MENSAJE FINAL";
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(101, 192);
+			this.textBox1.Location = new System.Drawing.Point(49, 192);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.Size = new System.Drawing.Size(241, 20);
 			this.textBox1.TabIndex = 5;
 			// 
 			// textBox2
 			// 
-			this.textBox2.Location = new System.Drawing.Point(101, 277);
+			this.textBox2.Location = new System.Drawing.Point(49, 277);
 			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.Size = new System.Drawing.Size(241, 20);
 			this.textBox2.TabIndex = 6;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(239, 307);
+			this.button1.Location = new System.Drawing.Point(134, 349);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 7;
-			this.button1.Text = "button1";
+			this.button1.Text = "GUARDAR";
 			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(90, 110);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker1.TabIndex = 8;
 			// 
 			// Configuracion
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(337, 351);
+			this.ClientSize = new System.Drawing.Size(348, 394);
+			this.Controls.Add(this.dateTimePicker1);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
@@ -136,5 +145,6 @@
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
 	}
 }
--- Chat/Form1.Designer.cs
+++ Chat/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+			this.btn_login = new System.Windows.Forms.Button();
+			this.Btn_Configuracion = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btn_login
+			// 
+			this.btn_login.Location = new System.Drawing.Point(12, 12);
+			this.btn_login.Name = "btn_login";
+			this.btn_login.Size = new System.Drawing.Size(97, 33);
+			this.btn_login.TabIndex = 0;
+			this.btn_login.Text = "Login";
+			this.btn_login.UseVisualStyleBackColor = true;
+			// 
+			// Btn_Configuracion
+			// 
+			this.Btn_Configuracion.Location = new System.Drawing.Point(175, 12);
+			this.Btn_Configuracion.Name = "Btn_Configuracion";
+			this.Btn_Configuracion.Size = new System.Drawing.Size(104, 33);
+			this.Btn_Configuracion.TabIndex = 1;
+			this.Btn_Configuracion.Text = "Configuración";
+			this.Btn_Configuracion.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(291, 74);
+			this.Controls.Add(this.Btn_Configuracion);
+			this.Controls.Add(this.btn_login);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.Button btn_login;
+		private System.Windows.Forms.Button Btn_Configuracion;
+	}
 }
 
